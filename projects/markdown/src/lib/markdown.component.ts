b345ee1ea4a8e2ed9846c6e72982bdd46cd6ef8d
sizer <shinse.tanaka@gmail.com>
2020-08-08 22:14:27 +0900

    generate markdown as library
    `yarn run ng g library markdown`

---
+++ projects/markdown/src/lib/markdown.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-markdown',
+  template: `
+    <p>
+      markdown works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class MarkdownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/markdown/src/lib/markdown.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { MarkdownComponent } from './markdown.component';
+
+
+
+@NgModule({
+  declarations: [MarkdownComponent],
+  imports: [
+  ],
+  exports: [MarkdownComponent]
+})
+export class MarkdownModule { }
