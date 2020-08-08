3a47595047b5b725fef2aad689795e2af9d10037
sizer <shinse.tanaka@gmail.com>
2020-08-08 19:37:54 +0900

    sync input and preview

--- projects/app/src/app/app.module.ts
+++ projects/app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +8,7 @@ import { DemoPageComponent } from './pages/demo-page/demo-page.component';
 
 @NgModule({
   declarations: [AppComponent, DemoPageComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [AppRoutingModule, BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- projects/app/src/app/pages/demo-page/demo-page.component.ts
+++ projects/app/src/app/pages/demo-page/demo-page.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-demo-page',
   templateUrl: './demo-page.component.html',
-  styleUrls: ['./demo-page.component.scss']
+  styleUrls: ['./demo-page.component.scss'],
 })
 export class DemoPageComponent implements OnInit {
+  public content = new FormControl('');
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
