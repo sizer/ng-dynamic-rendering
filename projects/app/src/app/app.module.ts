35f43198870b165f19e88267451f920dfe60f109
sizer <shinse.tanaka@gmail.com>
2020-08-08 23:13:29 +0900

    place MarkdownRenderer on DemoPage

--- projects/app/src/app/app.module.ts
+++ projects/app/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DemoPageComponent } from './pages/demo-page/demo-page.component';
+import { MarkdownModule } from '../../../markdown/src/lib/markdown.module';
 
 @NgModule({
   declarations: [AppComponent, DemoPageComponent],
-  imports: [AppRoutingModule, BrowserModule, ReactiveFormsModule],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    MarkdownModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
