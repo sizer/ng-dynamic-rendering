11cf5930e74510971a117429b009e866e10c049d
sizer <shinse.tanaka@gmail.com>
2020-08-08 19:20:17 +0900

    generate DemoPage

--- projects/app/src/app/app.module.ts
+++ projects/app/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemoPageComponent } from './pages/demo-page/demo-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, DemoPageComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
