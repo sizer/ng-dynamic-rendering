206a62f7689a27a02d1540019fff1de5c6897b0d
sizer <shinse.tanaka@gmail.com>
2020-08-08 19:21:02 +0900

    routing DemoPage as /

--- projects/app/src/app/app-routing.module.ts
+++ projects/app/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DemoPageComponent } from './pages/demo-page/demo-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DemoPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
