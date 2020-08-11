c97187218b7fb83b471d45e203b294278d63d5c8
sizer <shinse.tanaka@gmail.com>
2020-08-12 00:48:13 +0900

    display row input

--- projects/markdown/src/lib/markdown-renderer/markdown-renderer.component.ts
+++ projects/markdown/src/lib/markdown-renderer/markdown-renderer.component.ts
@@ -1,15 +1,12 @@
-import { Component, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+import { Component, ChangeDetectorRef, Input } from '@angular/core';
 
 @Component({
   selector: 'lib-markdown-renderer',
   templateUrl: './markdown-renderer.component.html',
   styleUrls: ['./markdown-renderer.component.scss'],
 })
-export class MarkdownRendererComponent implements AfterViewInit {
-  @ViewChild('c') public content: ElementRef;
-  constructor() {}
+export class MarkdownRendererComponent {
+  @Input() public md = '';
 
-  public ngAfterViewInit(): void {
-    console.log(this.content.nativeElement.textContent);
-  }
+  constructor(private cdRef: ChangeDetectorRef) {}
 }
--- projects/markdown/src/lib/markdown.module.ts
+++ projects/markdown/src/lib/markdown.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MarkdownRendererComponent } from './markdown-renderer/markdown-renderer.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [MarkdownRendererComponent],
-  imports: [],
+  imports: [BrowserModule],
   exports: [MarkdownRendererComponent],
 })
 export class MarkdownModule {}
